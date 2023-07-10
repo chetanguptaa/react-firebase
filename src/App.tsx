9d519ecacae0517ba6710f7fe05ad96c7e24d1a5
chetan gupta <chetangupta123raj@gmail.com>
2023-07-10 23:19:40 +0530

    updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import { Login } from './pages/Login';
 import { Main } from './pages/Main';
 import { Navbar } from './components/Navbar';
+import { CreatePost } from './pages/create-post/Create-Post';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Main />} />
           <Route path='/login' element={<Login />} />
+          <Route path='/createpost' element={<CreatePost />} />
         </Routes>
       </Router>
     </div>
